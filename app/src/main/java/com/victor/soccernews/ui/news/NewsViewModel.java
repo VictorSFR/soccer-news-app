5b56d3bcc19f4a1c31d5a69d41bb37b6ff48fbeb
VictorSFR <victorsfranca3@outlook.com>
2022-09-02 15:19:54 -0300

    Inclusão do Retrofit e Picasso; Consumo da mock API; Tela de notícias finalizada.

--- app/src/main/java/com/victor/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/victor/soccernews/ui/news/NewsViewModel.java
@@ -5,23 +5,45 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.victor.soccernews.domain.News;
+import com.victor.soccernews.domain.remote.SoccerNewsAPI;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> mNews;
+    private final MutableLiveData<List<News>> mNews = new MutableLiveData<>();
+    private final SoccerNewsAPI api;
 
     public NewsViewModel() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://victorsfr.github.io/soccer-news-api/").addConverterFactory(GsonConverterFactory.create()).build();
+        api = retrofit.create(SoccerNewsAPI.class);
+        this.findNews();
+    }
 
-        //TODO remover mock de noticias
-        mNews = new MutableLiveData<>();
-        List<News> news = new ArrayList<>();
-        news.add(new News("Flamengo derrota Palmeiras", "Descrição do negócio"));
-        news.add(new News("Botafogo derrota Vasco", "Descrição do negócio"));
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()){
+                    mNews.setValue(response.body());
+                } else{
+                    //TODO tratamento de erros
 
-        this.mNews.setValue(news);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                //TODO tratamento de erros
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
